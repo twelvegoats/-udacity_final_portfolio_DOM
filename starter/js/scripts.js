894f90b30360fe6a0a1aee5cfef041160766673b
Sean Wildman <twelvegoats@protonmail.com>
2025-08-14 08:46:11 -0400

    Fetch aboutMe data

--- starter/js/scripts.js
+++ starter/js/scripts.js
@@ -1,2 +1,9 @@
 // Fetch and populate About Me section
-async function fetchAboutMe() {}
+async function fetchAboutMe() {
+    try {
+        // Fetch the about me data from JSON file
+        const response = await fetch('../data/about-me.json');
+        // Parse the JSON response into a JavaScript object
+        const data = await response.json();
+    }
+}
